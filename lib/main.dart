819aa2042de6c7a94c1fb902397c1aeadb941ac4
GUILAVOGUI <sayonguilavogui518@yahoo.com>
2021-07-24 17:17:37 +0800

    t4

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 // import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:personal_expenses_app/widgets/chart.dart';
 import 'package:personal_expenses_app/widgets/new_transaction.dart';
 import 'package:personal_expenses_app/widgets/transaction_list.dart';
 
 import '../widgets/new_transaction.dart';
 import '../widgets/transaction_list.dart';
+import '../widgets/chart.dart';
 import '../models/transaction.dart';
 
 void main() => runApp(MyApp());
@@ -59,6 +61,16 @@ class _MyHomePageState extends State<MyHomePage> {
     // ),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
         title: txTitle,
@@ -99,14 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
